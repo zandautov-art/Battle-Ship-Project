9421a978670d123cf10b020b730ed58e7762e685
zandautov-art <zandautov@gmail.com>
2026-05-30 20:16:41 +0900

    Update GameInterface.fs

--- GameInterface.fs
+++ GameInterface.fs
@@ -3,14 +3,14 @@ namespace CS220
 open System
 
 /// Tic Tac Toe game interface.
-type GameInterface (player, board) =
-  member val PlayerMark: Marker = player with get
-  member val Board: Board = board with get
+type GameInterface (player, opponent) =
+  member this.p = player
+  member this.o = opponent
 
   member __.ReadNextMove () =
     printfn "Choose the next move for %s" <| Marker.toString player
     Console.Write ("> ")
     let idx = try Console.ReadLine () |> Convert.ToInt32 with _ -> -1
-    if idx >= 1 && idx <= 9 && not (board.IsOccupied idx) then idx
+    if idx >= 1 && idx <= 9 && not (this.p.IsOccupied idx) then idx
     else Console.WriteLine ("\n[*] Invalid input is given.\n")
          __.ReadNextMove ()
