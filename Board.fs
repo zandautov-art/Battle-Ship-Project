fbdcb2c728e7ea10c3668e8189f427edbaae2322
zandautov-art <zandautov@gmail.com>
2026-05-30 23:07:23 +0900

    Update Board.fs

--- Board.fs
+++ Board.fs
@@ -2,6 +2,7 @@ namespace CS220
 
 /// Board represents a 3x3 game board.
 type Board (int, bool) =
+  let rnd = System.Random()
 
   let mutable states = Array2D.create 6 6 EmptySlot
   let mutable g = 0
