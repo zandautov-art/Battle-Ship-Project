1449e7934e0e065232a5716b44e988e77a49d710
zandautov-art <zandautov@gmail.com>
2026-05-30 20:38:19 +0900

    Update GameInterface.fs

--- GameInterface.fs
+++ GameInterface.fs
@@ -6,6 +6,11 @@ open System
 type GameInterface (player, opponent) =
   member this.p = player
   member this.o = opponent
+  member __.CheckWinner () =
+      match (this.p.Cell, this.o.Cell) with =
+        |(36,_) -> Some opponent
+        |(_,36) -> Some player
+        | _ -> None
 
   member __.ReadNextMove () =
     printfn "Choose the next move for %s" <| Marker.toString player
@@ -14,3 +19,4 @@ type GameInterface (player, opponent) =
     if idx >= 1 && idx <= 9 && not (this.p.IsOccupied idx) then idx
     else Console.WriteLine ("\n[*] Invalid input is given.\n")
          __.ReadNextMove ()
+  
