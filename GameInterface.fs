404afec5f48cbf977b48244cdc973a2d0145a6c7
zandautov-art <zandautov@gmail.com>
2026-05-31 15:11:58 +0900

    Update GameInterface.fs

--- GameInterface.fs
+++ GameInterface.fs
@@ -7,7 +7,7 @@ type GameInterface (player, opponent) =
   member this.p = player
   member this.o = opponent
   member __.CheckWinner () =
-      match (this.p.Cell, this.o.Cell) with =
+      match (this.p.Cell, this.o.Cell) with 
         |(36,_) -> Some opponent
         |(_,36) -> Some player
         | _ -> None
@@ -16,8 +16,8 @@ type GameInterface (player, opponent) =
     printfn "Choose the next move for %s" <| player.toString
     Console.Write ("> ")
     let idx = try Console.ReadLine () 
-    let a = int( string idx.[1]))
-    let b = int( string idx.[3]))
+    let a = int( (string idx.[1]))
+    let b = int( (string idx.[3]))
     if a >= 1 && a <= 9 && b >= 1 && b <= 9 && not (this.p.IsOccupied idx) then (a, b)
     else Console.WriteLine ("\n[*] Invalid input is given.\n")
          __.ReadNextMove ()
